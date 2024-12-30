c27e26088645ad7c969b4a0fab14791d141568b7
ahemforGod <adepateenoch@gmail.com>
2024-12-30 15:56:19 +0100

    eleventh commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// const newColor = element.innerHTML("h1");
+// newColor.textContent = "boy";
+// console.log(newColor);
+// document.querySelector("h1").addEventListener("click", () => {
+//     alert("Ouch! Stop poking me!");
+//   });
+
+  const burger = document.getElementsByClassName(menu-button);
+  burger.addEventListener("click", ()=> {
+     location.reload()
+  })
