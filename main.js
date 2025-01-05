f16d44c1b02083025b9fecd5c0c13a642080e01b
ahemforGod <adepateenoch@gmail.com>
2025-01-05 18:01:11 +0100

    fifteenth commit

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
   const burger = document.querySelector(".burger");
+  const nav = document.querySelector("nav");
+  nav.addEventListener("click", reload = ()=> {
+   nav.style.display = "block";
+});
   burger.addEventListener("click", reload = ()=> {
      location.reload();
   });
